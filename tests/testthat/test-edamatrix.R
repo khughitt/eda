9736ca0d550377243740df89f11267279d8edcb8
Keith Hughitt <keith.hughitt@gmail.com>
2018-03-23 10:13:10 -0400

    Added initial testing code using testthat

---
+++ tests/testthat/test-edamatrix.R
@@ -0,0 +1,31 @@
+context("test-eda.R")
+
+# ensure reproducibility
+set.seed(1)
+
+# generate a test dataset
+num_rows <- 10
+num_cols <- 5
+
+mat <- matrix(rnorm(num_rows * num_cols), ncol=num_cols)
+rownames(mat) <- sprintf('obs%02d', 1:num_rows)
+colnames(mat) <- sprintf('var%02d', 1:num_cols)
+
+# row and column metadata
+row_mdat <- data.frame(obs_prop1=sample(10, num_rows, replace=TRUE),
+                       obs_prop2=factor(sample(letters[1:5], num_rows, replace=TRUE)))
+rownames(row_mdat) <- rownames(mat)
+
+col_mdat <- data.frame(var_prop1=sample(5, num_cols, replace=TRUE),
+                       var_prop2=factor(sample(c(T,F), num_cols, replace=TRUE)),
+                       var_prop3=rnorm(num_cols))
+rownames(col_mdat) <- colnames(mat)
+
+# create EDAMatrix
+edm <- EDAMatrix$new(mat, row_mdata=row_mdat, col_mdata=col_mdat)
+
+test_that("initialization works", {
+  expect_equal(edm$dat, mat)
+  expect_equal(edm$row_mdata, row_mdat)
+  expect_equal(edm$col_mdata, col_mdat)
+})
